a782ab0cd30003fe2e28fdc932c58397ff4aafc7
ycl <1149989444@qq.com>
2024-01-02 14:29:00 +0800

    提交测试

---
+++ src/main/java/com/itheima/reggie_take_out/controller/EmptyController.java
@@ -0,0 +1,15 @@
+package com.itheima.reggie_take_out.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class Name: EmptyController
+ * Description:
+ *
+ * @Author 原常乐
+ * @Create 2024/1/2 14:28
+ * @Version 1.0
+ */
+@RestController
+public class EmptyController {
+}
